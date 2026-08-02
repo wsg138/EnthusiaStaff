956a671253310672b368686badfa4f1a7361f944
P2wn <racecarboy77@gmail.com>
2026-08-02 10:41:45 -0400

    Extend sanction mutation store for exact sanctions

--- domain/src/main/java/net/enthusia/staff/domain/ports/SanctionMutationStore.java
+++ domain/src/main/java/net/enthusia/staff/domain/ports/SanctionMutationStore.java
@@ -1,8 +1,16 @@
 package net.enthusia.staff.domain.ports;
 
+import net.enthusia.staff.domain.sanction.ExactSanctionChangeRequest;
+import net.enthusia.staff.domain.sanction.ExactSanctionChangeResult;
+import net.enthusia.staff.domain.sanction.SanctionActionLimits;
 import net.enthusia.staff.domain.sanction.SanctionChangeRequest;
 import net.enthusia.staff.domain.sanction.SanctionChangeResult;
 
 public interface SanctionMutationStore {
     SanctionChangeResult apply(SanctionChangeRequest request);
+
+    ExactSanctionChangeResult applyExact(
+            ExactSanctionChangeRequest request,
+            SanctionActionLimits limits
+    );
 }
