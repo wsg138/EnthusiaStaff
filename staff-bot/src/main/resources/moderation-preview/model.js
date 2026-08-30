47eb305db81d708ef8fed54b071af984087cb66a
P2wn <racecarboy77@gmail.com>
2026-08-29 22:53:59 -0400

    Keep DOM replacement helper side-effect free

--- staff-bot/src/main/resources/moderation-preview/model.js
+++ staff-bot/src/main/resources/moderation-preview/model.js
@@ -121,8 +121,6 @@ function replaceChildrenOf(parent, ...children) {
   const fragment = document.createDocumentFragment();
   appendChildren(fragment, children);
   parent.replaceChildren(fragment);
-  parent.scrollTop = 0;
-  parent.scrollLeft = 0;
 }
 
 function textNode(tag, className, text) {
